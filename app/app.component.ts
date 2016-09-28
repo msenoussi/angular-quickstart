c0547bc72af70620eabd747076c89aa3d49ad64e
msenoussi <msenoussi@mynt.fr>
2016-09-28 15:40:04 +0200

    Add app html

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,9 +4,26 @@
     import { Component } from '@angular/core';
     @Component({
         selector: 'my-app',
-        template: `
+        /*template: `
                 <h1>Salut à tous</h1>
                 <p>ioj,jo,ni</p>
-                `
+                `*/
+        templateUrl: 'app/app.html'
     })
-    export class AppComponent { }
+    export class AppComponent {
+    private title: string;
+    private text: string;
+        private number: number;
+        private collection: [any]
+
+        constructor(){
+            this.title = "Salut !";
+            this.text = "Wech bien ou bien ?";
+            this.number = 0;
+            this.collection=[
+                {reference : '1234', nom: 'mohamed',state:0},
+                {reference: '2367', nom : 'sam',state:1},
+                {reference: '9000', nom :'guigui',state:2}
+            ]
+        }
+    }
